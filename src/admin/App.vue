65dcf817580c440d610b3b28507153618f5ccea5
Tanja <rezetat@gmail.com>
2019-04-05 19:59:38 +0300

    week 3 to check

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -30,8 +30,59 @@
             ul.skills-list
               li.skills-list__item
                 .skills__card-title__input
-                  //- label.input__card
-                  //-   input.input__elem(placeholder="Название новой группы")
+                  admin-input(
+                      type="text"
+                      placeholder="Название новой группы"
+                      v-model="todo.name"
+                      :errorText="validation.firstError('todo.name')"
+                      @keydown.enter="addNewTodo"
+                    )
+                  .skills__card-title__buttons
+                    .skills__card-title__btn
+                      button.btn.is-tick.
+                    .skills__card-title__btn
+                      button.btn.is-cross.
+                .card__content
+                  .skill-list__table
+                    .skills__table-container
+                      table.skills
+                          tr.skills__row
+                            td.skills__name 
+                            td.skills__percent 
+                            td.skills__buttons
+                          tr.skills__row
+                            td.skills__name 
+                            td.skills__percent 
+                            td.skills__buttons
+                          tr.skills__row
+                            td.skills__name 
+                            td.skills__percent 
+                            td.skills__buttons
+                          tr.skills__row
+                            td.skills__name 
+                            td.skills__percent 
+                            td.skills__buttons
+                    .skills__new.input__new-add
+                      admin-input(
+                        type="text"
+                        placeholder="Новый навык"
+                        v-model="todo.name"
+                        :errorText="validation.firstError('todo.name')"
+                        @keydown.enter="addNewTodo"
+                      )
+                      admin-input(
+                        type="text"
+                        placeholder="100%"
+                        v-model="todo.name"
+                        :errorText="validation.firstError('todo.name')"
+                        @keydown.enter="addNewTodo"
+                      )
+                      button.skills__new-add(
+                        type="submit"
+                        :disabled="disableSubmit"
+                      )
+              li.skills-list__item
+                .skills__card-title__input
                   .skills__card-title__text Workflow
                   .skills__card-title__buttons
                     .skills__card-title__btn
@@ -41,45 +92,95 @@
                 .card__content
                   .skill-list__table
                     .skills__table-container
-                      .skills
-                        .skills__row
-                          .skills__name Git
-                          .skills__percent 100%
-                          .skills__buttons
-                            .skills__btn
-                              button.btn.is-pencil
-                            .skills__btn
-                              button.btn.is-trash
-                        .skills__row
-                          .skills__name Git
-                          .skills__percent 100%
-                          .skills__buttons
-                            .skills__btn
-                              button.btn.is-pencil
-                            .skills__btn
-                              button.btn.is-trash
-                        .skills__row
-                          .skills__name Git
-                          .skills__percent 100%
-                          .skills__buttons
-                            .skills__btn
-                              button.btn.is-pencil
-                            .skills__btn
-                              button.btn.is-trash
-                        .skills__row
-                          .skills__name Git
-                          .skills__percent 100%
-                          .skills__buttons
-                            .skills__btn
-                              button.btn.is-pencil
-                            .skills__btn
-                              button.btn.is-trash
+                      table.skills
+                          tr.skills__row
+                            td.skills__name Git
+                            td.skills__percent 100 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash
+                          tr.skills__row
+                            td.skills__name Terminal
+                            td.skills__percent 90 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash
+                          tr.skills__row
+                            td.skills__name Galp
+                            td.skills__percent 80 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash
+                          tr.skills__row
+                            td.skills__name Webpack
+                            td.skills__percent 85 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash      
                     .skills__new
                       label.input__new
                         input.elem__new(placeholder="Новый навык")
                       label.percent__new
                         input.elem__percent(placeholder="100%")
                       button.skills__new-add 
+              li.skills-list__item
+                .skills__card-title__input
+                  .skills__card-title__text Frontend
+                  .skills__card-title__buttons
+                    .skills__card-title__btn
+                      button.btn.is-tick.no-words
+                    .skills__card-title__btn
+                      button.btn.is-cross.no-words
+                .card__content
+                  .skill-list__table
+                    .skills__table-container
+                      table.skills
+                          tr.skills__row
+                            td.skills__name HTML5 
+                            td.skills__percent 100 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash
+                          tr.skills__row
+                            td.skills__name CSS3
+                            td.skills__percent 90 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash
+                          tr.skills__row
+                            td.skills__name Javascript
+                            td.skills__percent 80 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash
+                          tr.skills__row
+                            td.skills__name Jquery и Vue.js
+                            td.skills__percent 85 %
+                            td.skills__buttons
+                              .skills__btn
+                                button.btn.is-pencil
+                              .skills__btn
+                                button.btn.is-trash      
+                    .skills__new
+                      label.input__new
+                        input.elem__new(placeholder="Новый навык")
+                      label.percent__new
+                        input.elem__percent(placeholder="100%")
+                      button.skills__new-add         
       section.work
         .container
           .work__page-title Блок "Работы"
@@ -90,6 +191,9 @@
                 .work__new-ipg
                   .work__new-ipg__text Перетащите или загрузите для загрузки изображения
                   button.work__new-ipg__add Загрузить
+                .work__new-ipg__tablets
+                    .work__new-img__tab
+                    button.work__new-ipg__tablets-add Изменить превью
               .work__new-content__right
                 .work__new-desc
                   label.form-block
@@ -181,7 +285,7 @@
                   label.form-block.form-block__textarea-reviews
                     span.form-block__name Отзывы
                     textarea.form__input.form__input-textarea(placeholder="Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!")
-                .work__new-buttons
+                .work__new-buttons.reviews__new-buttons
                   button.work__new-buttons__delete Отмена
                   button.work__new-buttons__save Сохранить
           .reviews__all
@@ -197,7 +301,7 @@
                       .user__name-name Владимир Сабанцев
                       .user__name-job Преподаватель
                   .reviews__desc Этот код выдержит любые испытания. Только пожалуйста, не загружайте сайт на слишком старых браузерах
-                  .work__all-buttons
+                  .work__all-buttons.reviews__all-buttons
                     button.btn__correct Править
                     button.btn__delete Удалить
                 li.reviews__all-list__item
@@ -217,16 +321,95 @@
                       .user__name-name Владимир Сабанцев
                       .user__name-job Преподаватель
                   .reviews__desc Этот код выдержит любые испытания. Только пожалуйста, не загружайте сайт на слишком старых браузерах
-                  .work__all-buttons
+                  .work__all-buttons.reviews__all-buttons
                     button.btn__correct Править
                     button.btn__delete Удалить
-
-
-
-
+      .login
+        .login__content
+          button(type="button").login__form-close
+          form.login__form(@submit.prevent="login")
+            h1.login__form-title Авторизация
+            .login__row
+              app-input(
+                title="Логин"
+                icon="user"
+                v-model="user.name"
+                :errorText="validation.firstError('user.name')"
+              )
+            .login__row
+              app-input(
+                title="Пароль"
+                icon="key"
+                type="password"
+                v-model="user.password"
+                :errorText="validation.firstError('user.password')"
+              )
+            .login__btn
+              button(
+                type="submit"
+                :disabled="disableSubmit"
+              ).login__send-data Отправить
 
 
 </template>                          
+
+ <script>
+import { Validator } from "simple-vue-validator";
+import axios from "axios";
+let uniqId = 0;
+export default {
+  mixins: [require("simple-vue-validator").mixin],
+  validators: {
+    "user.name": value => {
+      return Validator.value(value).required("Введите имя пользователя");
+    },
+    "user.password": value => {
+      return Validator.value(value).required("Введите пароль");
+    },
+    "todo.name":(value) =>{
+      return Validator.value(value).required("Поле не может быть пустым");
+    },
+  },
+  data() {
+    return {
+      disableSubmit: false,
+      user: {
+        name: "",
+        password: ""
+      },
+      todo: {
+        name: "",
+      },
+    };
+  },
+  components: {
+    appInput: () => import("./components/input"),
+    adminInput:() => import("./components/admin-input")
+  },
+  methods: {
+    async login() {
+      if ((await this.$validate()) === false) return;
+      this.disableSubmit = true;
+
+      try {
+        axios
+          .post("//jsonplaceholder.typicode.com/posts", {
+            name: this.user.name,
+            password: this.user.password,
+            text: this.todo.name,
+          })
+          .then(response => {
+            const report = JSON.stringify(response, null, 2);
+            console.log(report);
+          });
+      } catch (error) {
+        console.log(error);
+      };
+    }
+  }
+};
+
+</script>
 
 <style lang="pcss">
 @import "normalize.css";
@@ -239,7 +422,11 @@
 @import "../styles/admin/about.pcss";
 @import "../styles/admin/work.pcss";
 @import "../styles/admin/reviews.pcss";
+@import "../styles/admin/login.pcss";
 </style>
+
+
+
 
 
 
